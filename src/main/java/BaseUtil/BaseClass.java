0e592b8267725b4e0fa7c41c0203d954df1bf1a2
TofaelKabir <tofaelkabir@gmail.com>
2022-12-27 22:51:39 -0500

    Automation Framework Day03

--- src/main/java/BaseUtil/BaseClass.java
+++ src/main/java/BaseUtil/BaseClass.java
@@ -15,12 +15,22 @@ public class BaseClass {
 	
 	@BeforeMethod
 	public void setUp (){
+		
+		/*
+		// System is a class and setProperty is a method
+		// 1st way, to show the location of chrome driver
+		// This is an absolute path
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Tofael\\eclipse-workspace\\gov.portal.cms.oct\\driver\\chromedriver.exe");
+		*/
+		
+		// 2nd way, to show the location of chrome driver
+		// This is a dynamic way (relative path)
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/driver/chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.get("https://portal.cms.gov/portal/");
-		// We can also use fullscreen() instead of maximize()
-		driver.manage().window().fullscreen();
+		// Best is to use maximize()
+		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
+		driver.get("https://portal.cms.gov/portal/");
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		homePage = new HomePage(driver);
--- src/test/java/testPages/HomePageTest.java
+++ src/test/java/testPages/HomePageTest.java
@@ -1,9 +1,12 @@
 package testPages;
 
 import static common.CommonActions.*;
+
+import java.time.Duration;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
-import org.testng.reporters.EmailableReporter;
 
 import BaseUtil.BaseClass;
 
@@ -53,6 +56,7 @@ public class HomePageTest extends BaseClass{
 		homePage.forgotUserIdClick();
 	}
 	
+	// if we use try catch block in HomePage, we don't need to use it here, something new
 	@Test(enabled = false)
 	public void inputTextInUserIdFieldTest() {
 		homePage.inputTextInUserIdField();
@@ -99,21 +103,256 @@ public class HomePageTest extends BaseClass{
 	// Important: Use of cssSelector as locator
 	// cssSelector is very important for interview
 	// cssSelector by class --> inside the string, first put html tag, then dot, then write value of the class attribute 
-	@Test(enabled = true)
+	@Test(enabled = false)
 	public void role1_use_of_cssSelector_as_locator_in_new_user_registration_button () {
 		driver.findElement(By.cssSelector("a.cms-newuser-reg")).click();	
 	}
 	
 	// Web Element: logo
 	// Important: Use of cssSelector as locator
-	// if the class contain separate words [gh-tb ui-autocomplete-input], they are actually different class, then
+	// if the class contain separate words [cms-icon cms-sprite-loggedout ms-3], they are actually different class, then
 	// we have to close the gap between classes by putting dot/period between them
-	// cssSelector by class --> htmltag.class name value 
+	// cssSelector by class --> htmltag.class.name.value 
 	// remove the space between words in class value, and replace with dot or period
-	@Test(enabled = true)
-	public void role1_use_of_cssSelector_as_locator_in_logo () {
+	@Test(enabled = false)
+	public void role2_use_of_cssSelector_as_locator_in_logo () {
 			driver.findElement(By.cssSelector("em.cms-icon.cms-sprite-loggedout.ms-3")).click();
 	}
+	
+	// Important: Use of cssSelector as locator
+	// Web Element: login Button
+	// if you wanna create cssSelector by id 
+	// first html tag, then # (hash), then write the value of id attribute inside the string
+	@Test(enabled = false)
+	public void role3_use_of_cssSelector_as_locator_in_login_button() {
+		driver.findElement(By.cssSelector("button#cms-login-submit")).click();
+	}
+	
+	// Use of cssSelector as locator
+	// Web Element: login Button
+	// high level, can ignore
+	// cssSelector by another attribute
+	// --> htmltag[any attribute name='value'] , in xpath - you use // and @, which is absent here 
+	// and you can use attribute except id and class
+	@Test(enabled = false)
+	public void role4_use_of_cssSelector_as_locator_in_login_button () {
+		driver.findElement(By.cssSelector("button[name='Submit Login']")).click();
+	}
+	
+	// Important: Use of cssSelector as locator
+	// Web Element: login Button
+	// Not important, can ignore
+	// cssSelector by another attribute
+	// --> htmltag.value of class[attribute name='value'] , in xpath you use // and @, which is absent here and you can use attribute except id and class
+	@Test(enabled = false)
+	public void role5_use_of_cssSelector_as_locator_in_login_button() {
+		driver.findElement(By.cssSelector("button.cms-green-button-reg[name='Submit Login']")).click();
+	}
+	
+	// Important: Use of cssSelector as locator
+	// Web Element: login Button
+	// Not important, can ignore
+	// cssSelector by another attribute
+	// --> htmltag#value of ID[attribute name='value'] , in xpath you use // and @, which is absent here and you can use attribute except id and class
+	@Test(enabled = false)
+	public void role6_use_of_cssSelector_as_locator_in_login_button () {
+		driver.findElement(By.cssSelector("button#cms-login-submit[name='Submit Login']")).click();
+	}
+	
+	// Important: Use of tagName as locator
+	// tag name: we have to go to Amazon and look for table
+	
+	// Web Element: logo
+	// isDisplayed() is an alternate of click() method which is boolean type
+	// isDisplayed() is the method used to verify the presence of a web element within the web page.
+	// Use of isDisplayed() available- image, link, button, text field, check box etc.
+   @Test(enabled = false)
+	public void use_of_isDisplayed_in_logo_image() {
+		boolean elementDisplayed = driver.findElement(By.cssSelector("em.cms-icon.cms-sprite-loggedout.ms-3")).isDisplayed();
+		System.out.println("Is the LOGO displayed? Ans: " + elementDisplayed);
+	}
+
+   	// Web Element: login Button
+	// isEnabled() is the method used to verify if the web element is enabled or
+	// disabled within the web page. isEnabled() is primarily used with buttons.
+	// Use of isEnabled(), a boolean type method
+	
+	@Test(enabled = false)
+	public void use_of_isEnabled_in_login_button() throws InterruptedException {
+		boolean loginButtonEnabled = driver.findElement(By.id("cms-login-submit")).isEnabled();
+		System.out.println("Is the Login Button Enable? Ans: " + loginButtonEnabled);
+	}
+	
+	// Web Element: New User Registration
+	// no need next time
+	// another example
+	@Test(enabled = false)
+	public void newUserRegistrationTest() {
+		boolean nurButtonEnabled = driver.findElement(By.xpath("//a[text()='New User Registration']")).isEnabled();
+		System.out.println("Is the New User Registration Button Enable? Ans: " + nurButtonEnabled);
+	}
+	
+    // Both is not working
+	// Web Element: check box
+	@Test (enabled = false)
+	public void use_of_isSelected_in_check_box () {
+		boolean checkBoxSelected = driver.findElement(By.xpath("//label[@id='cms-label-tc']")).isSelected();
+		System.out.println("Is the check Box selected? Ans: " + checkBoxSelected);
+	}
+
+	// Web Element: Radio button in Forgot User ID 
+	@Test (enabled = false)
+	public void use_of_isSelected_in_radio_button () throws InterruptedException {
+		// first click on Forgot User ID
+		homePage.forgotUserIdClick();
+		Thread.sleep(5000);
+		boolean radioButtonSelected = driver.findElement(By.xpath("(//label[@class='check-radio'])[1]")).isSelected();
+		System.out.println("Is the check Radio Button selected? Ans: " + radioButtonSelected);
+	}
+	
+	@Test(enabled = false)
+	public void logoDisplayedTest() {
+		homePage.logoDisplayed();
+	}
+	
+	@Test (enabled = false)
+	public void clickUnlockTest() {
+		homePage.clickUnlock();
+	}
+	
+	// We use New User Registration
+	// how can you find the title of the page?
+	// Basically inside <head> tag, <title> tag is present, you can get it from there or
+	// right click on an empty place of any page -- click "view page source" -- ctrl+f (to find) 
+	// -- Type 'title' -- find the <title> tag
+	@Test(enabled = false)
+	public void use_of_getTitle_method_in_home_page() {
+		System.out.println("The Title of the Page is: " + driver.getTitle());
+	}
+	
+	// We use New User Registration
+	@Test(enabled = false)
+	public void use_of_getTitle_method_in_nur_page() throws InterruptedException {
+		driver.findElement(By.className("cms-newuser-reg")).click();
+		Thread.sleep(5000);
+		System.out.println("The Title of the Page is: " + driver.getTitle());
+	}
+	
+	// We use New User Registration	
+	@Test(enabled = false)
+	public void use_of_getCurrentURL_method () throws InterruptedException {
+		driver.findElement(By.className("cms-newuser-reg")).click();
+		Thread.sleep(5000);
+		System.out.println("The Title of the Page is: " + driver.getTitle());
+		System.out.println("The current URL is: " + driver.getCurrentUrl());
+	}
+	
+	// We use New User Registration web element to see the text of the web element
+	@Test(enabled = false)
+	public void use_of_getText_method () {
+		WebElement nur = driver.findElement(By.className("cms-newuser-reg"));
+		System.out.println("The Text for the Web Element is: " + nur.getText());
+	}
+	
+	// In User id no text is present, let's see what it shows as text? -- Empty
+	@Test(enabled = false)
+	public void use_of_getText_method_in_userid () {
+		WebElement userId = driver.findElement(By.name("user-d"));
+		System.out.println("The Text for the Web Element is: " + userId.getText());
+	}
+	
+	// We use New User Registration
+	// getAttribute() actually give the value of the Attribute
+	@Test(enabled = false)
+	public void use_of_getAttribute_method () {
+		String value01 = driver.findElement(By.xpath("//a[contains(text(), 'New')]")).getAttribute("id");
+		String value02 = driver.findElement(By.xpath("//a[contains(text(), 'New')]")).getAttribute("class");
+		String value03 = driver.findElement(By.xpath("//a[contains(text(), 'New')]")).getAttribute("title");
+		String value04 = driver.findElement(By.xpath("//a[contains(text(), 'New')]")).getAttribute("href");
+		System.out.println("The value of the id attribute is: " + value01);
+		System.out.println("The value of the class attribute is: " + value02);
+		System.out.println("The value of the title attribute is: " + value03);
+		System.out.println("The value of the href attribute is: " + value04);
+	}
+	
+	// CMS doesn't have search field, so we will direct to Amazon page
+	@Test(enabled = true)
+	public void use_of_sendKeys_method_then_click_submit_and_then_clear() throws InterruptedException {
+		Thread.sleep(5000);
+		// directing to Amazon.com
+		driver.get("https://www.amazon.com/");
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		// Writing text in search field
+		driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys("Laptop");
+		Thread.sleep(5000);
+		// click on submit button
+		driver.findElement(By.xpath("//input[@id='nav-search-submit-button']")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).clear();
+		Thread.sleep(5000);
+		// Writing text in search field
+		driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys("iPad");
+		Thread.sleep(5000);
+		// click on submit button
+		driver.findElement(By.xpath("//input[@id='nav-search-submit-button']")).click();
+		Thread.sleep(5000);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
 	
 	
 	
